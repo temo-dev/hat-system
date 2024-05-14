0ce5ef9b99e6112eebfb6b9ceeb33086dc3e8e1f
TuanAnh-Seta <anhnt6879@seta-international.vn>
2024-05-14 10:12:45 +0200

    first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, NzIconModule, NzLayoutModule, NzMenuModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.less']
 })
 export class AppComponent {
-  title = 'hat-system';
+  isCollapsed = false;
 }
