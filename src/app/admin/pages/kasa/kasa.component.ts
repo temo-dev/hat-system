f3ee754e7b9b8c9dba79d85ba0c2e27d50c052e7
TuanAnh-Seta <anhnt6879@seta-international.vn>
2024-05-19 16:56:43 +0200

    add-search

--- src/app/admin/pages/kasa/kasa.component.ts
+++ src/app/admin/pages/kasa/kasa.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
 import { NzGridModule } from 'ng-zorro-antd/grid';
 import { OrderContainerComponent } from './components/order-container/order-container.component';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzFlexModule } from 'ng-zorro-antd/flex';
 
 @Component({
   selector: 'app-kasa',
   standalone: true,
-  imports: [NzGridModule, OrderContainerComponent],
+  imports: [
+    NzGridModule,
+    OrderContainerComponent,
+    NzInputModule,
+    NzButtonModule,
+    NzIconModule,
+    NzFlexModule,
+  ],
   templateUrl: './kasa.component.html',
   styleUrl: './kasa.component.less',
 })
